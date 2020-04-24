c03600f8b6657e5e4713fe2059e97b231787c483
Polkinghorn, Grant Christopher <grant.polkinghorn@gmail.com>
2020-04-24 13:17:34 -0500

    AppUpload
    Upload

---
+++ BarBuddySite/BarBuddySite/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BarBuddySite.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarBuddySite.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BarBuddySite/BarBuddySite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BarBuddySite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
